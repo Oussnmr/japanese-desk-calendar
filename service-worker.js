a47e93dbfe3f82733718d7fb0fa4a26b5a872491
Oussnmr <Oussama.nemri@hotmail.com>
2026-09-02 22:49:21 +0200

    Add subtle animated ink cloud behind date

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "japanese-desk-calendar-v3";
+const CACHE_NAME = "japanese-desk-calendar-v4";
 const OFFLINE_URL = new URL("./index.html", self.registration.scope).href;
 const LOCAL_ASSETS = [
   "./",
